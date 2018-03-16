8b133691d40821fa70eff0d3075929538df1e57d
Ramón Casero <rcasero@gmail.com>
2018-03-16 17:04:35 +0000

    add CUDA 9.0 with Patches 1 and 2 among versions that can be installed from nvidia website

--- bin/install_cuda.sh
+++ bin/install_cuda.sh
@@ -14,6 +14,9 @@
 #
 #      ubuntu_packages: (def) From Ubuntu official packages.
 #
+#      nvidia_9.0_ubuntu_17.04: From the Nvidia website, CUDA 9.0 .deb packages for Ubuntu 17.04 (x86_64),
+#                               with Patch 1 (Released Jan 25, 2018) and Patch 2 (Released Mar 5, 2018)
+#
 #      nvidia_9.1_ubuntu_16.04: From the Nvidia website, CUDA 9.1 .deb packages for Ubuntu 16.04 (x86_64).
 #
 #      nvidia_9.1_ubuntu_17.04: From the Nvidia website, CUDA 9.1 .deb packages for Ubuntu 17.04 (x86_64),
@@ -50,10 +53,12 @@ tput setaf 1; echo "** Installing Nvidia CUDA Toolkit"; tput sgr0
 
 case "$CUDA_TYPE"
 in
+
+    # For the Ubuntu packages, we do all the installation here, and then exit the script
     
     ubuntu_packages)
 	
-	# Option 1. CUDA Toolkit from the Ubuntu distribution packages
+	# CUDA Toolkit from the Ubuntu distribution packages
 	echo "** Remove Nvidia website packages, if present, as they take the same role as Ubuntu official packages"
 	set +e
 	sudo apt remove -y --purge cuda
@@ -69,26 +74,43 @@ in
 	
 	exit 0
 	;;
-    
-    nvidia_9.1_ubuntu_16.04)
 
-	# Option 2. From Nvidia website:
+    # For the packages directly from the NVIDIA website, here we only
+    # assign variable names, and then in the rest of the script we
+    # take care of the installation of the packages
+    
+    nvidia_9.0_ubuntu_17.04)
+	
+	# From Nvidia website:
+	# CUDA Toolkit 9.0 for Ubuntu 17.04
+	CUDA_VERSION=9.0
+	CUDA_PACKAGE=cuda-repo-ubuntu1704-9-0-local_9.0.176-1_amd64-deb
+	CUDA_URL=https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/${CUDA_PACKAGE}
+	;;
+
+    nvidia_9.1_ubuntu_16.04)
+	
+	# From Nvidia website:
 	# CUDA Toolkit 9.1 for Ubuntu 16.04
-	CUDA_VERSION=cuda-repo-ubuntu1604-9-1-local_9.1.85-1_amd64
+	CUDA_VERSION=9.1
+	CUDA_PACKAGE=cuda-repo-ubuntu1604-9-1-local_9.1.85-1_amd64
+	CUDA_URL=https://developer.nvidia.com/compute/cuda/9.1/Prod/local_installers/${CUDA_PACKAGE}
 	;;
     
     nvidia_9.1_ubuntu_17.04)
 
-	# Option 2. From Nvidia website:
+	# From Nvidia website:
 	# CUDA Toolkit 9.1 for Ubuntu 17.04
-	CUDA_VERSION=cuda-repo-ubuntu1704-9-1-local_9.1.85-1_amd64
+	CUDA_VERSION=9.1
+	CUDA_PACKAGE=cuda-repo-ubuntu1704-9-1-local_9.1.85-1_amd64
+	CUDA_URL=https://developer.nvidia.com/compute/cuda/9.1/Prod/local_installers/${CUDA_PACKAGE}
 	;;
 
-	*)
-	  
-	    echo "Error: Option not recognised: $CUDA_TYPE"
-	    exit 1
-	    ;;
+    *)
+	
+	echo "Error: Option not recognised: $CUDA_TYPE"
+	exit 1
+	;;
 esac
 
 ## Common code to NVIDA website CUDA Toolkit installation
@@ -101,86 +123,102 @@ sudo apt autoremove -y
 set -e
 
 echo
-echo "** Install Nvidia website packages"
+echo "** Install packages from Nvidia website"
 
-# check whether package cuda is already installed
-#
-# The following line of code produces 0 if the package is not listed
-# yet in APT or is not installed, and 1 if it's installed. E.g.,
-# assuming that the cuda package is installed
-#
-#   $ dpkg-query -l cuda 2>/dev/null
-#   Desired=Unknown/Install/Remove/Purge/Hold
-#   | Status=Not/Inst/Conf-files/Unpacked/halF-conf/Half-inst/trig-aWait/Trig-pend
-#   |/ Err?=(none)/Reinst-required (Status,Err: uppercase=bad)
-#   ||/ Name                    Version          Architecture     Description
-#   +++-=======================-================-================-====================================================
-#   ii  cuda                    9.1.85-1         amd64            CUDA meta-package
-#
-#   $ dpkg-query -l cuda 2>/dev/null| tail -n 1
-#   ii  cuda           9.1.85-1     amd64        CUDA meta-package
-#
-#   $ dpkg-query -l cuda 2>/dev/null| tail -n 1 | sed 's/  */ /g'
-#   ii cuda 9.1.85-1 amd64 CUDA meta-package
-#
-#   $ dpkg-query -l cuda 2>/dev/null| tail -n 1 | sed 's/  */ /g' | cut -f 3 -d ' '
-#   9.1.85-1
-#
-#   $ dpkg-query -l cuda 2>/dev/null| tail -n 1 | sed 's/  */ /g' | cut -f 3 -d ' ' | sed 's/[a-Z<>]//g'
-#   9.1.85-1
-#
-#   $ dpkg-query -l cuda 2>/dev/null | tail -n 1 | sed 's/  */ /g' | cut -f 3 -d ' ' | sed 's/[a-Z<>]//g' | wc -w
-#   1
+# check whether package cuda with the desired version is already installed
 
-CHECK=`dpkg-query -l cuda 2>/dev/null | tail -n 1 | sed 's/  */ /g' | cut -f 3 -d ' ' | sed 's/[a-Z<>]//g' | wc -w`
+set +e
+# we only want the major.minor part of the version number, e.g. 9.1.85-1 -> 9.1
+CUDA_PACKAGE_INSTALLED_VERSION=`dpkg-query --showformat='${Version}' --show cuda | grep -o '^[0-9]*\.[0-9]*'`
+set -e
 
-if [ $CHECK -eq 0 ]
+# if there's an installed cuda package, with the version we want, we can skip re-installation
+if [ ! -z $CUDA_PACKAGE_INSTALLED_VERSION ] && [ $CUDA_PACKAGE_INSTALLED_VERSION == $CUDA_VERSION ]
 then
+    echo "** cuda package already installed, skipping"
+
+else
+    echo "** Removing cuda package with undesired version"
+    set +e
+    sudo apt remove --purge -y cuda
+    sudo apt autoremove -y
+    sudo apt remove --purge cuda-repo-*
+    set -e
+    export CUDA_PACKAGE_INSTALLED_VERSION=""
+    
     echo "** Installing cuda package"
     pushd ~/Downloads
-    if [ ! -e "${CUDA_VERSION}.deb" ];
+    if [ ! -e "${CUDA_PACKAGE}.deb" ];
     then
-	wget https://developer.nvidia.com/compute/cuda/9.1/Prod/local_installers/${CUDA_VERSION}
-	mv ${CUDA_VERSION} ${CUDA_VERSION}.deb
+	wget ${CUDA_URL}
+	mv ${CUDA_PACKAGE} ${CUDA_PACKAGE}.deb
     fi
-    sudo dpkg -i ${CUDA_VERSION}.deb
-    sudo apt-key add /var/cuda-repo-9-1-local/7fa2af80.pub
-    sudo apt-get update
-    sudo apt-get install -y cuda
+    sudo dpkg -i ${CUDA_PACKAGE}.deb
+    case "$CUDA_VERSION"
+    in
+	9.0)
+	    sudo apt-key add /var/cuda-repo-9-0-local/7fa2af80.pub ;;
+	9.1)
+	    sudo apt-key add /var/cuda-repo-9-1-local/7fa2af80.pub ;;
+    esac
+    sudo apt update
+    sudo apt install -y cuda
 
     # add cuda to the path
     set +e
-    isInBashrc=`grep  -c "export PATH=/usr/local/cuda-9.1/bin" ~/.bashrc`
+    isInBashrc=`grep  -c "export PATH=/usr/local/cuda-${CUDA_VERSION}/bin" ~/.bashrc`
     set -e
     if [ "$isInBashrc" -eq 0 ];
     then
-	echo "Adding /usr/local/cuda-9.1/bin to PATH in ~/.bashrc"
+	echo "Adding /usr/local/cuda-${CUDA_VERSION}/bin to PATH in ~/.bashrc"
 	echo "
 # added by pysto/tools/install_cuda.sh
-export PATH=/usr/local/cuda-9.1/bin:\"\$PATH\"" >> ~/.bashrc
+export PATH=/usr/local/cuda-${CUDA_VERSION}/bin:\"\$PATH\"" >> ~/.bashrc
 	source ~/.bashrc
     else
-	echo "/usr/local/cuda-9.1/bin already in PATH in ~/.bashrc"
+	echo "/usr/local/cuda-${CUDA_VERSION}/bin already in PATH in ~/.bashrc"
     fi
        
     popd
-else
-    echo "** cuda package already installed, skipping"
 fi
 
-# install patch to version for Ubuntu 17.04
-if [ "${CUDA_VERSION}" == "cuda-repo-ubuntu1704-9-1-local_9.1.85-1_amd64" ];
-then 
-    echo "** Patching cuda package"
-    pushd ~/Downloads
-    if [ ! -e "cuda-repo-ubuntu1704-9-1-local-cublas-performance-update-1_1.0-1_amd64.deb" ];
-    then
-	wget https://developer.nvidia.com/compute/cuda/9.1/Prod/patches/1/cuda-repo-ubuntu1704-9-1-local-cublas-performance-update-1_1.0-1_amd64
-	mv cuda-repo-ubuntu1704-9-1-local-cublas-performance-update-1_1.0-1_amd64 cuda-repo-ubuntu1704-9-1-local-cublas-performance-update-1_1.0-1_amd64.deb
-    fi
-    sudo dpkg -i cuda-repo-ubuntu1704-9-1-local-cublas-performance-update-1_1.0-1_amd64.deb
-fi
+# install patches when available
+case "$CUDA_VERSION"
+in
+    9.0)
+	
+	echo "** Patching cuda 9.0 package"
+	pushd ~/Downloads
+	PATCH=cuda-repo-ubuntu1704-9-0-local-cublas-performance-update_1.0-1_amd64-deb
+	if [ ! -e "$PATCH.deb" ];
+	then
+	    wget https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/1/${PATCH}
+	    mv ${PATCH} ${PATCH}.deb
+	fi
+	sudo dpkg -i ${PATCH}.deb
+	PATCH=cuda-repo-ubuntu1704-9-0-local-cublas-performance-update-2_1.0-1_amd64-deb
+	if [ ! -e "$PATCH.deb" ];
+	then
+	    wget https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/2/${PATCH}
+	    mv ${PATCH} ${PATCH}.deb
+	fi
+	sudo dpkg -i ${PATCH}.deb
+	;;
+
+    9.1)
+	
+	echo "** Patching cuda 9.1 package"
+	pushd ~/Downloads
+	PATCH=cuda-repo-ubuntu1704-9-1-local-cublas-performance-update-1_1.0-1_amd64
+	if [ ! -e "$PATCH.deb" ];
+	then
+	    wget https://developer.nvidia.com/compute/cuda/9.1/Prod/patches/1/${PATCH}
+	    mv ${PATCH} ${PATCH}.deb
+	fi
+	sudo dpkg -i ${PATCH}.deb
+	;;
+esac
 
 # output which version of CUDA has been installed
 tput setaf 1; echo "** Installed CUDA version:"; tput sgr0
-nvcc --version
+/usr/local/cuda-${CUDA_VERSION}/bin/nvcc --version
